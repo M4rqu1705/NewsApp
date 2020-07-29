1e38e8c71763a0486a891827f3fdee3f40ca5a29
M4rqu1705 <m4rc05.dev@gmail.com>
2020-07-28 20:53:19 -0400

    💄📰 Improved layout and completed backend with Primera Hora, Metro and Claridad

--- app/static/script.js
+++ app/static/script.js
@@ -2,11 +2,14 @@
 // Thank you https://scotch.io/tutorials/how-to-change-a-css-background-images-opacity
 
 
-var api_server = "https://marcos-newsapp.herokuapp.com";
-var MAX_ARTICLES = 5;
 var overlay = document.getElementById("overlay");
 var max_articles_input = document.getElementById("max_articles");
+var keywords_input = document.getElementById("keyword_search")
 var container = document.getElementById("container");
+// var api_server = "https://marcos-newsapp.herokuapp.com";
+var api_server = "http://127.0.0.1:5000";
+var MAX_ARTICLES = max_articles_input.value;
+var previous_content;
 
 function overlay_on() {
     overlay.style.display = "flex";
@@ -25,34 +28,110 @@ function overlay_toggle() {
     }
 }
 
-function update_max_articles() {
-    MAX_ARTICLES = max_articles_input.value;
+function dropdown_on(el) {
+    el.nextElementSibling.classList.add('show');
+}
+function dropdown_off(el) {
+    el.nextElementSibling.classList.remove('show');
     update_screen();
 }
+function dropdown_toggle(el) {
+    if (el.nextElementSibling.classList.contains('show')) {
+        dropdown_off(el);
+    } else {
+        dropdown_on(el);
+    }
+}
 
-function update_screen() {
+function frequency(string) {
+    words = string.split(" ");
+
+    freq = {}
+
+    for (let i = 0, l0 = words.length; i < l0; i++) {
+        if (freq[words[i]] !== undefined) {
+            freq[words[i]]++;
+        } else {
+            freq[words[i]] = 1;
+        }
+    }
+
+    return freq;
+}
+
+function update_max_articles() {
+    MAX_ARTICLES = max_articles_input.value;
+    if (MAX_ARTICLES !== '' && MAX_ARTICLES < 50) {
+        update_screen();
+    }
+}
+
+function update_screen(requestAgain = true) {
     container.innerHTML = "";
 
-    axios.get(`${api_server}/api/v1/services/news_scraping?papers=endi,vocero&max_articles=${MAX_ARTICLES}`)
-        .then(function (articles) {
-            console.log(articles)
+    // Prepare which news papers to filter for
+    let papers = [];
 
-            const keys = Object.keys(articles);
-            const l0 = keys.length;
-            for (let i = 0; i < l0; i++) {
+    const temp = document.getElementById("papersFilterDropdown").children;
+    for (let i = 0, l0 = temp.length; i < l0; i++) {
+        if (temp[i].tagName === "INPUT" && temp[i].checked) {
+            papers.push(temp[i].value);
+        }
+    }
 
-                const key = keys[i];
-                const l1 = articles[key].length;
-                for (let j = 0; j < l1; j++) {
+    function add_articles(articles) {
+        let keywords = keywords_input.value.trim();
 
-                    const article = articles[key][j];
+        if (keywords.includes(",")) {
+            keywords = keywords.split(",");
+        }
+
+        const keys = Object.keys(articles);
+        const l0 = keys.length;
+        for (let i = 0; i < l0; i++) {
+
+            const key = keys[i];
+            const l1 = articles[key].length;
+            for (let j = 0; j < l1; j++) {
+
+                const article = articles[key][j];
+
+                // Search for keywords
+                text = (article.title + article.subtitle + article.article).toLowerCase();
+
+                if (keywords === "") {
                     insertCard(article, paper = keys[i]);
+                } else if (typeof keywords === "string" && keywords !== "") {
+                    // Check if the keyword (the string) is in freq
+                    if (text.includes(keywords)) {
+                        insertCard(article, paper = keys[i]);
+                    }
+                } else if (typeof keywords === "object" && keywords !== "") {
+                    // Check if any of the keywords is in freq
+                    for (let j = 0, l1 = keywords.length; j < l1; j++) {
+                        if (text.includes(keywords[j])) {
+                            insertCard(article, paper = keys[i]);
+                            break;
+                        }
+                    }
                 }
             }
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        }
+    }
+
+    // Retrieve newspaper from backend
+    if (requestAgain) {
+        axios.get(`${api_server}/api/v1/services/news_scraping?papers=${papers.join(",")}&max_articles=${MAX_ARTICLES}`)
+            .then(function (articles) {
+                previous_content = articles;
+                add_articles(articles);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    } else {
+        add_articles(previous_content);
+    }
 }
 
 function insertCard(article, paper) {
@@ -80,17 +159,34 @@ function insertCard(article, paper) {
     card.appendChild(div);
 
     card.addEventListener("click", function (e) {
-        overlay_on();
+        // Hide any open dropdown menus
+        const temp = document.getElementsByClassName('dropdown-content');
+        const l0 = temp.length;
+        for (let i = 0; i < l0; i++) {
+            if (temp[i].classList.contains("show")) {
+                temp[i].classList.remove("show");
+            }
+        }
+
+        // Prepare content for the article
+        const x_icon = `<i class="fa fa-fw fa-times-circle overlay_close" onclick="overlay_off()"></i>`;
+
+        const title = `<h2 class="overlay_title">${article.title}</h2>`;
+        const subtitle = `<h4 class="overlay_subtitle">${article.subtitle}</h4>`;
+        const image = `<img class='overlay_image' src='${article.image}' alt='${article.title}'/>`
+        const date = `<h6 class='overlay_date'>${article.last_edited}</h6><hr>`;
+        const header = title + subtitle + image + date;
+
         const link_end = `<hr>Leer articulo original <a href="${article.url}">aqui</a>`;
 
-        const header = `<h2 class="overlay_title">${article.title}</h2>
-        <h4 class="overlay_subtitle">${article.subtitle}</h4>
-        <img class='overlay_image' src='${article.image}' alt='${article.title}'/>
-        <h6 class='overlay_date'>${article.last_edited}</h6><hr>`;
+        // Compose content
+        overlay.children[0].innerHTML = x_icon + header + article.article.replace(/\n/g, "<br>") + link_end;
 
-        overlay.children[0].innerHTML = header + article.article.replace(/\n\n/g, "<br><br>") + link_end;
+        // console.log(article.article);
+        overlay_on();
     });
 
+    // Blur animation to introduce new cards
     card.animate([
         { opacity: 0 },
         { opacity: 1 }
@@ -99,11 +195,7 @@ function insertCard(article, paper) {
         iterations: 1
     });
 
-    if (paper === "endi") {
-        card.classList.add("endi");
-    } else if (paper == "vocero") {
-        card.classList.add("vocero");
-    }
+    card.classList.add(paper);
 
     container.appendChild(card)
 }
--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Thank you https://beckernick.github.io/faster-web-scraping-python/
 
 import bs4
@@ -10,7 +12,8 @@ import concurrent.futures
 
 MAX_THREADS = 50
 DEBUG = False
-MAX_ARTICLES = 10
+SYNCHRONOUS = True
+MAX_ARTICLES = 100
 
 def retrieve_site(url):
     try:
@@ -21,6 +24,16 @@ def retrieve_site(url):
 
     except Exception:
         return None
+
+def sort_and_format(output):
+    output.sort(key=lambda x: x['last_edited'])
+    output = output[::-1]
+
+    for i, el in enumerate(output):
+        el['last_edited'] = el['last_edited'] - dt.timedelta(hours=4)
+        output[i]['last_edited'] = el['last_edited'].strftime("%b %d, %Y | %I:%M:%S %p")
+
+    return output
 
 def endi():
     output = []
@@ -92,19 +105,21 @@ def endi():
 
         threads = min(MAX_THREADS, MAX_ARTICLES)
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-            futures = executor.map(process_article, articles)
-            concurrent.futures.wait(list(filter(None, futures)))
+        if SYNCHRONOUS:
+            for i, article in enumerate(articles):
+                if DEBUG:
+                    print(f'{i}', end='')
+                process_article(article)
+        else:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+                futures = executor.map(process_article, articles)
+                concurrent.futures.wait(list(filter(None, futures)))
 
-        #  for i, article in enumerate(articles):
-            #  if DEBUG:
-                #  print(f'{i}', end='')
-            #  process_article(article)
-
-        return output
+        return sort_and_format(output)
 
     else:
         return None
+
 
 def vocero():
     output = []
@@ -172,131 +187,313 @@ def vocero():
 
         threads = min(MAX_THREADS, MAX_ARTICLES)
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-            futures = executor.map(process_article, articles)
-            concurrent.futures.wait(list(filter(None, futures)))
+        if SYNCHRONOUS:
+            for i, article in enumerate(articles):
+                if DEBUG:
+                    print(f'{i}', end='')
+                process_article(article)
+        else:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+                futures = executor.map(process_article, articles)
+                concurrent.futures.wait(list(filter(None, futures)))
 
-        #  for i, article in enumerate(articles):
-            #  if DEBUG:
-                #  print(f'{i}', end='')
-            #  process_article(article)
-
-        return output
+        return sort_and_format(output)
 
     else:
         return None
 
+
 def primera_hora():
-    response = retrieve_site('https://www.primerahora.com/arcio/news-sitemap/')
+    output = []
 
-    if response != None:
-        articles = xmltodict.parse(response.text)['urlset']['url']
+    def process_article(article):
+        # Retrieve information from sitemap
+        url = article['loc']
 
-        output = []
+        title = article['news:news']['news:title']
+        title = title.title()
 
-        for article in articles:
-            url = article['loc']
+        last_edited = dt.datetime.fromisoformat(article['lastmod'][:-1])
 
-            title = article['news:news']['news:title']
-            title = title.title()
+        #  published = dt.datetime.fromisoformat(article['news:news']['news:publication_date'][:-1])
+        #  news_name = article['news:news']['news:publication']['news:name']
+        #  news_language = article['news:news']['news:publication']['news:language']
+        #  changefreq = article['changefreq']
 
-            last_edited = dt.datetime.fromisoformat(article['lastmod'][:-1])
+        # Retrieve article's url to extract titles, image, and content
+        response = retrieve_site(url)
 
-            #  published = dt.datetime.fromisoformat(article['news:news']['news:publication_date'][:-1])
-            #  news_name = article['news:news']['news:publication']['news:name']
-            #  news_language = article['news:news']['news:publication']['news:language']
-            #  changefreq = article['changefreq']
+        if response != None and response.ok:
+            soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+            opinion = False
+            bs4_page_title = soup.select_one('head > title')
+            if "opinión" in bs4_page_title.text.lower():
+                opinion = True
+                return
+
+            article_body = soup.select_one('div.ArticleContent > main')
+            if article_body == None:
+                opinion = True
+                article_body = soup
+
+            # Retrieve title
+            bs4_title = article_body.select_one('h1.ArticleHeadline_head')
+            title = ''
+            if bs4_title != None:
+                title = bs4_title.text.strip()
+            else:
+                opinion = True
+                article_body = soup
+                bs4_title = article_body.select_one('h1.ArticleHeadline_head')
+                if bs4_title != None:
+                    title = bs4_title.text.strip()
+                else:
+                    return
+
+            # Retrieve subtitle
+            bs4_subtitle = article_body.select_one('h2.ArticleHeadline_subhead')
+            subtitle = ''
+            if bs4_subtitle != None:
+                subtitle = bs4_subtitle.text.strip()
+
+            # Retrieve image
+            if opinion:
+                bs4_image = soup.select_one('picture.AuthorBio__image > img.ResponsiveImage__img')
+                image_url = ''
+                if bs4_image != None:
+                    image_url = bs4_image['src']
+            else:
+                bs4_image = article_body.select_one('figure.ArticlePhotoMain > picture > img.ResponsiveImage__img')
+                image_url = ''
+                if bs4_image != None:
+                    image_url = bs4_image['src']
+
+                # It is assumed that article otherwise has video
+                
+
+            # Retrieve article content
+            bs4_article_body = article_body.select_one('section.ArticleBody')
+
+            bs4_p = bs4_article_body.select('p')
+            # Remove paragraphs which are simply links to other articles
+            bs4_p = [p for p in bs4_p if ['a'] != [child.name for child in p.children]]
+            content = '\n\n'.join([p.text.strip() for p in bs4_p if p.text.strip().lower() !=
+                "publicidad"])
 
             if DEBUG:
-                print(f' - {title} ({last_edited}) - {url}\n')
+                print(f' - {title[0:10]}… ({last_edited}) - {content[0:20]}… \n')
 
             output.append({
                 'title': title,
+                'subtitle': subtitle,
                 'last_edited': last_edited,
+                'image': image_url,
+                'article': content,
                 'url': url,
+                'paper': 'vocero'
                 })
 
-        return output
+    response = retrieve_site('https://www.primerahora.com/arcio/news-sitemap/')
+
+    if response != None and response.ok:
+        articles = xmltodict.parse(response.text)['urlset']['url']
+        if len(articles) > MAX_ARTICLES:
+            articles = articles[0:MAX_ARTICLES]
+
+        threads = min(MAX_THREADS, MAX_ARTICLES)
+
+        if SYNCHRONOUS:
+            for i, article in enumerate(articles):
+                if DEBUG:
+                    print(f'{i}', end='')
+                process_article(article)
+        else:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+                futures = executor.map(process_article, articles)
+                concurrent.futures.wait(list(filter(None, futures)))
+
+        return sort_and_format(output)
 
     else:
         return None
 
 
 def metropr():
-    response = retrieve_site('https://www.metro.pr/pr/sitemap/news-sitemap.xml')
+    output = []
 
-    if response != None:
-        articles = xmltodict.parse(response.text)['urlset']['url']
+    def process_article(article):
+        # Retrieve information from sitemap
+        url = article['loc']
 
-        output = []
+        title = article['n:news']['n:title']
+        title = title.title()
 
-        for article in articles:
-            url = article['loc']
+        last_edited = dt.datetime.fromisoformat(article['n:news']['n:publication_date'])
 
-            title = article['n:news']['n:title']
-            title = title.title()
+        #  image_url = article['image:image']['image:loc']
+        #  news_name = article['n:news']['n:publication']['n:name']
+        #  news_language = article['n:news']['n:publication']['n:language']
+        #  keywords = article['n:news']['n:keywords'].split(',')
 
-            last_edited = dt.datetime.fromisoformat(article['n:news']['n:publication_date'])
+        # Retrieve article's url to extract titles, image, and content
+        response = retrieve_site(url)
 
-            #  image_url = article['image:image']['image:loc']
-            #  news_name = article['n:news']['n:publication']['n:name']
-            #  news_language = article['n:news']['n:publication']['n:language']
-            #  keywords = article['n:news']['n:keywords'].split(',')
+        if response != None and response.ok:
+            soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
+            article_body = soup.select_one('article.main-article')
+
+            # Retrieve title
+            bs4_title = article_body.select_one('header.article-header > h1.article-title')
+            title = ''
+            if bs4_title != None:
+                title = bs4_title.text.strip()
+
+            # Retrieve subtitle
+            bs4_subtitle = article_body.select_one('header.article-header > h2.excerpt')
+            subtitle = ''
+            if bs4_subtitle != None:
+                subtitle = bs4_subtitle.text.strip()
+
+            # Retrieve image
+            bs4_image = article_body.select_one('img[data-backmedia="true"]')
+            image_url = ''
+            if bs4_image != None:
+                image_url = bs4_image['src']
+
+            # Retrieve article content
+            bs4_article_body = soup.select_one('div.resumen')
+
+            bs4_p = bs4_article_body.findAll(['p', 'h4'])
+            content = '\n\n'.join([p.text.strip() for p in bs4_p])
 
             if DEBUG:
-                print(f' - {title} ({last_edited}) - {url}\n')
+                print(f' - {title[0:10]}… ({last_edited}) - {content[0:20]}… \n')
 
             output.append({
-                'title': title,
+                'title': title.encode("iso-8859-1", "ignore").decode("utf-8", "ignore"),
+                'subtitle': subtitle.encode("iso-8859-1", "ignore").decode("utf-8", "ignore"),
                 'last_edited': last_edited,
+                'image': image_url,
+                'article': content.encode("iso-8859-1", "ignore").decode("utf-8", "ignore"),
                 'url': url,
+                'paper': 'vocero'
                 })
 
-        return output
+    response = retrieve_site('https://www.metro.pr/pr/sitemap/news-sitemap.xml')
+
+    if response != None and response.ok:
+        articles = xmltodict.parse(response.text)['urlset']['url']
+        if len(articles) > MAX_ARTICLES:
+            articles = articles[0:MAX_ARTICLES]
+
+        threads = min(MAX_THREADS, MAX_ARTICLES)
+
+        if SYNCHRONOUS:
+            for i, article in enumerate(articles):
+                if DEBUG:
+                    print(f'{i}', end='')
+                process_article(article)
+        else:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+                futures = executor.map(process_article, articles)
+                concurrent.futures.wait(list(filter(None, futures)))
+
+        return sort_and_format(output)
 
     else:
         return None
 
 
 def claridad():
-    temp = dt.datetime.now()
-    year, month = temp.year, str(temp.month).zfill(2)
+    output = []
 
-    response = retrieve_site(f'https://www.claridadpuertorico.com/sitemap-pt-post-{year}-{month}.xml')
+    def process_article(article):
+        # Retrieve information from sitemap
+        url = article['loc']
+        last_edited = dt.datetime.fromisoformat(article['lastmod'])
 
-    if response != None:
-        articles = xmltodict.parse(response.text)['urlset']['url']
+        #  changefreq = article['changefreq']
+        #  priority = article['priority']
 
-        output = []
+        # Retrieve article's url to extract titles, image, and content
+        response = retrieve_site(url)
 
-        for article in articles:
-            url = article['loc']
+        if response != None and response.ok:
+            soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
-            title = article['loc'].split('/')[-2]
-            while '-' in title:
-                title = title.replace('-', ' ')
-            title = title.title()
+            # Retrieve title
+            bs4_title = soup.select_one('header.entry-header > h1')
+            title = ''
+            if bs4_title != None:
+                title = bs4_title.text.strip()
 
-            last_edited = dt.datetime.fromisoformat(article['lastmod'])
+            # There is no subtitle
+            subtitle = ''
 
-            #  changefreq = article['changefreq']
-            #  priority = article['priority']
+            # Retrieve image
+            bs4_image = soup.select_one('main.site-main > article.post > div > img')
+            image_url = ''
+            if bs4_image != None:
+                image_url = bs4_image['src']
+            else:
+                return
 
+            # Retrieve article content
+            bs4_article_body = soup.select_one('div.entry-content')
+
+            bs4_p = bs4_article_body.findAll(['p', 'ul'])
+            content = '\n\n'.join([p.text.strip() for p in bs4_p])
+            #  breakpoint()
+
+
+            # Use some initial words as subtitle
+            subtitle = content[0:50].split("/")[0] + "…"
 
             if DEBUG:
-                print(f' - {title} ({last_edited}) - {url}\n')
+                print(f' - {title[0:10]}… ({last_edited}) - {content[0:20]}… \n')
 
             output.append({
                 'title': title,
+                'subtitle': subtitle,
                 'last_edited': last_edited,
+                'image': image_url,
+                'article': content,
                 'url': url,
+                'paper': 'vocero'
                 })
 
-        return output
+    year = dt.datetime.now().year
+    month = dt.datetime.now().month
+    
+    year = str(year).zfill(4)
+    month = str(month).zfill(2)
+
+    response = retrieve_site(f'https://www.claridadpuertorico.com/sitemap-pt-post-{year}-{month}.xml')
+
+    if response != None and response.ok:
+        articles = xmltodict.parse(response.text)['urlset']['url']
+        if len(articles) > MAX_ARTICLES:
+            articles = articles[0:MAX_ARTICLES]
+
+        threads = min(MAX_THREADS, MAX_ARTICLES)
+
+        if SYNCHRONOUS:
+            for i, article in enumerate(articles):
+                if DEBUG:
+                    print(f'{i}', end='')
+                process_article(article)
+        else:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+                futures = executor.map(process_article, articles)
+                concurrent.futures.wait(list(filter(None, futures)))
+
+        return sort_and_format(output)
 
     else:
         return None
+
 
 FUNCTIONS = {
         'endi': endi,
@@ -312,9 +509,8 @@ if __name__ == "__main__":
     outputs = {}
 
     #  outputs['endi'] = endi()
-    outputs['vocero'] = vocero()
-    breakpoint()
-    outputs['primera_hora'] = primera_hora()
+    #  outputs['vocero'] = vocero()
+    #  outputs['primera_hora'] = primera_hora()
     # MAYBE NOTICEL (Es un revolú)
-    outputs['metropr'] = metropr()
-    outputs['claridad'] = claridad()
+    #  outputs['metropr'] = metropr()
+    #  outputs['claridad'] = claridad()
